efb13f4d44cc6439ce1489b43f351fe1da2a15cd
Gahyun Lee <ab3380@naver.com>
2022-11-05 15:29:18 +0900

    first commit

---
+++ src/main/java/org/baedareun/minjok/KnowalcoholApplication.java
@@ -0,0 +1,13 @@
+package org.baedareun.minjok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowalcoholApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowalcoholApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/baedareun/minjok/KnowalcoholApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baedareun.minjok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowalcoholApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
