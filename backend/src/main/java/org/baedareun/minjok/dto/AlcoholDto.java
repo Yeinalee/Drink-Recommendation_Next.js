e96a8ea1ab86b03cc930ac1983755a25a4f84e45
Gongmeda <gongmeda@gmail.com>
2022-11-06 06:26:11 +0900

    fix(backend): alcohol DTO 수정

--- backend/src/main/java/org/baedareun/minjok/dto/AlcoholDto.java
+++ backend/src/main/java/org/baedareun/minjok/dto/AlcoholDto.java
@@ -7,10 +7,11 @@ import org.baedareun.minjok.entity.Alcohol;
 @Getter
 @AllArgsConstructor
 public class AlcoholDto {
+    private final int id;
     private final String name;
     private final String photoKey;
 
     public static AlcoholDto of(Alcohol alcohol) {
-        return new AlcoholDto(alcohol.getName(), "static/alcohols/" + alcohol.getPhoto());
+        return new AlcoholDto(alcohol.getId(), alcohol.getName(), "static/alcohols/" + alcohol.getPhoto());
     }
 }
