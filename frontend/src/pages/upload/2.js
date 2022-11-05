c9fb033d8cb6be1a7ba8fa159a5a6454af851d56
Kangdyu <kangdyu99@gmail.com>
2022-11-06 04:18:24 +0900

    feat(frontend): upload step2

--- frontend/src/pages/upload/2.js
+++ frontend/src/pages/upload/2.js
@@ -1,9 +1,18 @@
 import { Textarea } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
 import SearchSectionLayout from "../../components/pages/search/SearchSectionLayout";
+import { LOCAL_STORAGE_KEY } from "../../constants/localStorage";
 
 function Upload2Page() {
   const router = useRouter();
+
+  const [description, setDescription] = useState("");
+
+  const handleClickNextButton = useCallback(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY.UPLOAD_DESCRIPTION_KEY, description);
+    router.push("/upload/3");
+  }, [router, description]);
 
   return (
     <SearchSectionLayout
@@ -12,13 +21,14 @@ function Upload2Page() {
       onClickPrevButton={() => {
         router.push("/upload/1");
       }}
-      onClickNextButton={() => {
-        router.push("/upload/3");
-      }}
+      onClickNextButton={handleClickNextButton}
       buttonText="다음 단계로"
+      disableButton={description.length <= 0}
     >
       <Textarea
         size="md"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
         placeholder="레시피 설명"
         fontSize={"20px"}
         focusBorderColor="white"
@@ -26,7 +36,7 @@ function Upload2Page() {
         borderRadius={"24px"}
         padding={"12px 16px 12px 16px"}
         h="200px"
-        w="390px"
+        resize="none"
       />
     </SearchSectionLayout>
   );
