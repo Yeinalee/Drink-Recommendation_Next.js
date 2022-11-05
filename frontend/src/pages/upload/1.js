b9fcb5cdc9a5627d834aa206c466d1533e5fbb56
Kangdyu <kangdyu99@gmail.com>
2022-11-06 04:10:55 +0900

    feat(frontend): upload step1

--- frontend/src/pages/upload/1.js
+++ frontend/src/pages/upload/1.js
@@ -1,9 +1,18 @@
-import { Box, Input } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useCallback, useState } from "react";
 import SearchSectionLayout from "../../components/pages/search/SearchSectionLayout";
+import { LOCAL_STORAGE_KEY } from "../../constants/localStorage";
 
 function UploadStartPage() {
   const router = useRouter();
+
+  const [name, setName] = useState("");
+
+  const handleClickNextButton = useCallback(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY.UPLOAD_NAME_KEY, name);
+    router.push("/upload/2");
+  }, [router, name]);
 
   return (
     <SearchSectionLayout
@@ -12,20 +21,20 @@ function UploadStartPage() {
       onClickPrevButton={() => {
         router.push("/");
       }}
-      onClickNextButton={() => {
-        router.push("/upload/2");
-      }}
+      onClickNextButton={handleClickNextButton}
       buttonText="다음 단계로"
+      disableButton={name.length <= 0}
     >
       <Input
-        size="md"
+        size="lg"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
         placeholder="레시피 이름"
         fontSize={"20px"}
         focusBorderColor="white"
         backgroundColor="white"
         borderRadius={100}
-        h="48px"
-        w="390px"
+        w="100%"
       />
     </SearchSectionLayout>
   );
