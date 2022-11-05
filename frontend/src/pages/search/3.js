82fa657a1d8b7826efcc46c099505fea4a7fb165
Kangdyu <kangdyu99@gmail.com>
2022-11-06 03:06:01 +0900

    feat(frontend): search step 3 상호작용 (except search button)

--- frontend/src/pages/search/3.js
+++ frontend/src/pages/search/3.js
@@ -4,9 +4,21 @@ import { Search2Icon } from "@chakra-ui/icons";
 import SearchSectionLayout from "../../components/pages/search/SearchSectionLayout";
 import SimpleDrinkCard from "../../components/common/SimpleDrinkCard";
 import { MOCKUP_KIND_OF_DRINKS } from "../../mockups/kindOfDrink";
+import { useCallback, useState } from "react";
 
-function TagSearchPage() {
+function IngredientsSearchPage() {
   const router = useRouter();
+
+  const [ingredients, setIngredients] = useState(
+    MOCKUP_KIND_OF_DRINKS.map((kind) => ({ ...kind, selected: false }))
+  );
+
+  const [selectedCount, setSelectedCount] = useState(0);
+
+  const handleClickNextButton = useCallback(() => {
+    const selectedIngredients = ingredients.filter((ingredient) => ingredient.selected);
+    router.push("/search");
+  }, [router, ingredients]);
 
   return (
     <SearchSectionLayout
@@ -15,9 +27,7 @@ function TagSearchPage() {
       onClickPrevButton={() => {
         router.push("/search/2");
       }}
-      onClickNextButton={() => {
-        router.push("/search");
-      }}
+      onClickNextButton={handleClickNextButton}
       buttonText="검색하기"
     >
       <InputGroup size="lg" alignItems="center" marginBottom="20px">
@@ -27,12 +37,33 @@ function TagSearchPage() {
       </InputGroup>
 
       <SimpleGrid columns={2} spacing="20px">
-        {MOCKUP_KIND_OF_DRINKS.map((kind, idx) => (
-          <SimpleDrinkCard key={idx} name={kind.name} imageSrc={kind.photo} />
+        {ingredients.map((ingredient, idx) => (
+          <SimpleDrinkCard
+            key={idx}
+            onClick={() => {
+              if (ingredient.selected) {
+                setSelectedCount(selectedCount - 1);
+              } else {
+                setSelectedCount(selectedCount + 1);
+              }
+
+              setIngredients([
+                ...ingredients.slice(0, idx),
+                { ...ingredient, selected: !ingredient.selected },
+                ...ingredients.slice(idx + 1, ingredients.length),
+              ]);
+            }}
+            _hover={{
+              cursor: "pointer",
+            }}
+            selected={ingredient.selected}
+            name={ingredient.name}
+            imageSrc={ingredient.photo}
+          />
         ))}
       </SimpleGrid>
     </SearchSectionLayout>
   );
 }
 
-export default TagSearchPage;
+export default IngredientsSearchPage;
