ee31858dc4a35286064f3858b786236a94ebbc1a
wnsgud0428 <wnsgud0428@naver.com>
2022-11-06 06:32:39 +0900

    chore(backend): tag type 데이터 추가

--- backend/src/main/java/org/baedareun/minjok/enums/TagType.java
+++ backend/src/main/java/org/baedareun/minjok/enums/TagType.java
@@ -4,8 +4,19 @@ import java.util.Arrays;
 import java.util.Objects;
 
 public enum TagType {
+    DOSU_HIGH("도수 높은"),
+    DOSU_LOW("도수 낮은"),
     SOFT("부드러운"),
-    SWEET("달달한");
+    SWEET("달달한"),
+    SAECOM("새콤한"),
+    TOK("톡 쏘는"),
+    SALTY("쌉쌀한"),
+    WITH_FRIENDS("친구들끼리"),
+    WITH_LOVER("연인끼리"),
+    WITH_FAMILY("가족끼리"),
+    ALONE("혼자");
+
+
 
     final private String name;
 
