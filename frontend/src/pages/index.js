ae4cee95d5a0e322e8a36f597f22f2a34848bbfe
Kangdyu <kangdyu99@gmail.com>
2022-11-05 21:07:11 +0900

    feat(frontend): 주류 카드 추가

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,4 +1,6 @@
 import { Container, Heading } from "@chakra-ui/react";
+import DrinkCard from "../components/common/DrinkCard";
+import drinkImage from "/public/images/drink.png";
 
 function HomePage() {
   return (
@@ -6,6 +8,13 @@ function HomePage() {
       <Heading size="lg" color="primary">
         알는체
       </Heading>
+      <DrinkCard
+        imageSrc={drinkImage}
+        name="레시피"
+        description="한줄설명한줄설명한줄설명한줄설명한줄설명"
+        likeCount={10}
+        commentCount={10}
+      />
     </Container>
   );
 }
